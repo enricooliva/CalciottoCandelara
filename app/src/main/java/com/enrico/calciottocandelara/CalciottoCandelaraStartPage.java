859b97e7be5c875854151aac41ea6a90be0d88ef
Enrico Oliva <oliva.enrico@gmail.com>
2015-04-13 00:41:13 +0200

    Aggiunti layout landscape, icone e splash

--- app/src/main/java/com/enrico/calciottocandelara/CalciottoCandelaraStartPage.java
+++ app/src/main/java/com/enrico/calciottocandelara/CalciottoCandelaraStartPage.java
@@ -1,27 +1,52 @@
 package com.enrico.calciottocandelara;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RatingBar;
 
 
-public class CalciottoCandelaraStartPage extends ActionBarActivity {
+public class CalciottoCandelaraStartPage extends Activity {
 
     private static final int PLAYERS_ID = 0;
     private static final int GAMES_ID = 1;
     private static final int TEAM_ID = 2;
     public static final int INFO=3;
     private RatingBar teamIndex;
+    private boolean mTwoPane;
 
+    // Splash screen timer
+    private static int SPLASH_TIME_OUT = 3000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calciotto_candelara_start_page);
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(CalciottoCandelaraStartPage.this, PlayersActivity.class);
+                startActivity(i);
+
+                // close this activity
+                //finish();
+            }
+        }, SPLASH_TIME_OUT);
+
+
     }
 
 
--- app/src/main/java/com/enrico/calciottocandelara/PlayersActivity.java
+++ app/src/main/java/com/enrico/calciottocandelara/PlayersActivity.java
@@ -1,12 +1,22 @@
 package com.enrico.calciottocandelara;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RatingBar;
 
 
 public class PlayersActivity extends ActionBarActivity {
+
+    private static final int HOME_ID = 0;
+    private static final int GAMES_ID = 1;
+    private static final int TEAM_ID = 2;
+    public static final int INFO=3;
+    private RatingBar teamIndex;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,11 @@ public class PlayersActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_players, menu);
+        menu.add(Menu.NONE,HOME_ID, Menu.NONE, R.string.menuitem_logo);
+        menu.add(Menu.NONE,GAMES_ID, Menu.NONE, R.string.menuitem_games);
+        menu.add(Menu.NONE,TEAM_ID, Menu.NONE, R.string.menuitem_team);
+
+        menu.add(Menu.NONE,INFO, Menu.NONE, R.string.menuitem_info);
         return true;
     }
 
@@ -38,8 +53,41 @@ public class PlayersActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             return true;
         }
+        switch	(item.getItemId())
+        {
+
+            case HOME_ID:
+                //aprire activity players list
+                Intent splash = new Intent(PlayersActivity.this,CalciottoCandelaraStartPage.class);
+                startActivity(splash);
+                return true;
+
+            case GAMES_ID:
+                //aprire activity players list
+                Intent gamelist = new Intent(PlayersActivity.this,GamesActivity.class);
+                startActivity(gamelist);
+                return true;
+
+            case TEAM_ID:
+                Intent team = new Intent(PlayersActivity.this,TeamRateActivity.class);
+                startActivity(team);
+                return true;
+            case  INFO:
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+
+                alert.setTitle("Info Calc8 Candelara");
+                alert.setMessage(""
+                        + "Calc8 Candelara \n\n"
+                        + "Vota le prestazioni dei giocatori \n"
+                        + "e guarda l'indicatore dello stato di forma della squadra."
+                        + "\n\nhttp://www.facebook.com/candelara.calciotto");
+                alert.show();
+                return true;
+
+        }
 
         return super.onOptionsItemSelected(item);
+
     }
 
 
